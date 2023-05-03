902774347068aa157caf530f9ef4761f17d35ce9
Rohitkumar0042 <132429259+Rohitkumar0042@users.noreply.github.com>
2023-05-03 11:21:14 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 2363700
 api_hash = "527cf5174eigfkggigig3611bc69d7b7709"
-bot_token = "6180767554gjggkgkgifBsrDmKItUewgdKGAtGo"
+bot_token = "5871947270:AAHcDm97MWLDFtvbMd-7TxdAu86fs1Im3r8"
 
